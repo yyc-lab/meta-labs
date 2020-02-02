db1877c77beb53cc275a54e92025624bbcb7a80b
venetrius <gergelygjuhasz@gmail.com>
2020-02-02 12:37:13 -0800

    ProjectPage - Retreive and render projects

--- frontend/src/pages/Projects/Projects.js
+++ frontend/src/pages/Projects/Projects.js
@@ -1,14 +1,51 @@
-import React from 'react'
 import {useGlobal} from '../../state'
 
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Axios from 'axios'
+const backendEndpoint = 'http://localhost:3030'
+
+const renderProjects = (isLoading, projects) => {
+  if(isLoading){
+    return "please wait until we load it for you"
+  }else if(! projects){
+    return "No project is found"
+  }
+  else{
+    return projects.map(project => 
+      <div>
+        <h2>{project.name}</h2>
+        {project.repos.map(repo => <a href={repo}> GitHub </a>)}
+        <p>{project.description}</p>
+        <p>Estimated length: {project.time_line}</p>
+        <p>{project.tech_stack.join(", ")}</p>
+      </div>
+      )
+  }
+}
+
 export const Projects = () => {
-  const [projects] = useGlobal('projects')
-  return (
-    <div>
-      Projects Page
-      {projects.map(project => (
-        <div key={project}>{project}</div>
-      ))}
-    </div>
-  )
+  const [ global, setGlobal ] = useGlobal()
+  const [isLoading, setIsLoading] = useState(true) 
+  const [err, setErr] = useState(null)
+  const [data, setData] = useState(global.projects)
+  const token = global.token
+
+  const login = () => Axios.get(`${backendEndpoint}/auth/github`);
+
+  useEffect(() => {
+    if(!global.projects) {
+      Axios.get(`${backendEndpoint}/api/projects`)
+      .then((data) => {
+        setGlobal({ ...global, projects: data.projects })
+        setIsLoading(false);
+        setData(data.data);
+      })
+      .catch((err) => {
+        setErr(err);
+      });
+    } 
+  }, []);
+  // TODO should use Project component
+  return renderProjects(isLoading, data)
 }
