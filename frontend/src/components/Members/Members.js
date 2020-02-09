343baaba16bd37a6621a76e58e8c5983431553c9
cpfeiffer325 <cpfeiffer325@gmail.com>
2020-02-08 20:56:05 -0700

    create Members component for Project info Page

---
+++ frontend/src/components/Members/Members.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card } from '../../external_components'
+
+export const Members = () => {
+  return (
+    <div>
+      <Card title="Members" style={{ height: 200 }}>
+        Member Icons and + Button
+      </Card>
+    </div>
+  )
+}
