25c0a036b111f01ba134f1ba5b8e7068c4801654
cpfeiffer325 <cpfeiffer325@gmail.com>
2020-02-08 20:55:50 -0700

    create Documents component for Project info Page

---
+++ frontend/src/components/Documents/Documents.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card } from '../../external_components'
+
+export const Documents = () => {
+  return (
+    <div>
+      <Card title="Documents" style={{ height: 300 }}>
+        List of Documents added, Line Items for each document
+      </Card>
+    </div>
+  )
+}
