51162c94afcb99838ac608e4acdb8daf1154645c
Gergely Juhasz <gergelyjuhasz@Gergelys-MacBook-Pro.local>
2020-02-20 18:04:39 -0700

    add Links to header

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
 import { useGlobal } from '../../state';
 import { Layout } from '../../external_components'
 
@@ -6,13 +10,18 @@ const LayoutHeader = Layout.Header;
 export const Header = () => {
   const [user] = useGlobal('user');
   // if (!user) return null; // TODO: Add it back in once connected with login
+  let authPart = user ?  (<div className="user">
+  username:
+  <button> Signout </button> </div>) :       <Link to="/login">Login</Link>
+
+
   return (
     <LayoutHeader className="App-header">
       <h3 className="title"> YYCLabs </h3>
-      <div className="user"> 
-        username: 
-        <button> Signout </button>
-      </div>
+      <Link to="/projects">Projects</Link>
+      <Link to="/dashboard">Dashboard</Link>
+      {authPart}
+
     </LayoutHeader>
   )
 }
