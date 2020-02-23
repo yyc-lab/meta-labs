eb743a3a89fcec410732719945e846385290ec34
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2020-02-22 21:26:01 -0700

    fix linter styles for login page

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,48 +1,49 @@
 import React, { useState } from 'react';
-import { Redirect } from 'react-router'
-import Logo from '../App/lhl_logo.png'
-import Axios from 'axios';
-
-// import Button from '../../external_components'
-import 'antd/dist/antd.css';
-import { Button } from 'antd'; // TODO CONNECT TO EXTERNAL COMPONENTS
-import './login.css'
+import { Redirect } from 'react-router';
 import qs from 'query-string';
+import Axios from 'axios';
+import { Button } from 'antd'; // TODO CONNECT TO EXTERNAL COMPONENTS
+import 'antd/dist/antd.css';
+
+import './login.css';
+import Logo from '../App/lhl_logo.png';
 import { useGlobal } from '../../state';
 
 const backendEndpoint = 'http://localhost:3022';
 
 const getTokenFromQuery = () => {
   const queryValues = qs.parse(window.location.search);
-  return queryValues.token
-}
+  return queryValues.token;
+};
 
 export const Login = () => {
-  const [loginRedirect, setLoginRedirect] = useState(false)
-  const [loadingSpinner, setLoadingSpinner] = useState(false)
-  const [loadUser, setLoadUser] = useState(false)
-  const [ global, setGlobal, user ] = useGlobal()
+  const [loginRedirect, setLoginRedirect] = useState(false);
+  const [loadingSpinner, setLoadingSpinner] = useState(false);
+  const [loadUser, setLoadUser] = useState(false);
+  const [global, setGlobal, user] = useGlobal();
+
+  const token = getTokenFromQuery();
 
   function fetchUser() {
-    setLoadUser(true)
+    setLoadUser(true);
     Axios.get(`${backendEndpoint}/api/user/profile`, { headers: { Authorization: `Bearer ${token}` } })
-    .then((res) => {
-      setGlobal({ ...global, user: res.data.data })
-      setLoadUser(false)
-    })
-    .catch(() => setLoadUser(false))
+      .then((res) => {
+        setGlobal({ ...global, user: res.data.data });
+        setLoadUser(false);
+      })
+      .catch(() => setLoadUser(false));
   }
 
-  if(global.token) {
-    return <Redirect to="/projects" />
+  if (global.token) {
+    return <Redirect to="/projects" />;
   }
-  const token = getTokenFromQuery()
+
 
   if (token) {
     if (!user && !loadUser) {
-      fetchUser()
+      fetchUser();
     }
-    setGlobal({...global, token, loginRedirect: true})
+    setGlobal({ ...global, token, loginRedirect: true });
   }
 
   if (loadingSpinner) {
@@ -52,22 +53,21 @@ export const Login = () => {
           Loading
         </Button>
       </div>
-    )
+    );
   }
 
-  if(loginRedirect) {
-    return <Redirect to="/dashboard" />
+  if (loginRedirect) {
+    return <Redirect to="/dashboard" />;
   }
 
   return (
     <main id="login-page">
       <div className="logo">YYC Labs</div>
-     {/*} <button onClick={loginClick} className="github">Login with Github</button> */}
-     <a href={process.env.REACT_APP_GITHUB_OAUTH_URL}>
-      Login
-     </a>
+      <a href={process.env.REACT_APP_GITHUB_OAUTH_URL}>
+        Login
+      </a>
       <p>A GitHub account is required to login to YYC Labs.</p>
-      <img src={Logo} height="40"/>
+      <img alt="logo" src={Logo} height="40" />
     </main>
-  )
-}
+  );
+};
