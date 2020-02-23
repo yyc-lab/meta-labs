e1dc08b4108b9a17ab071664005e269d1a367ec3
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2020-02-22 21:25:02 -0700

    redirect app depending on user

--- frontend/src/pages/App/App.js
+++ frontend/src/pages/App/App.js
@@ -1,36 +1,39 @@
 import React from 'react';
-import { Dashboard } from '../Dashboard'
-import { NotFound } from '../NotFound'
-import { NewProject } from '../NewProject'
-import { Project } from '../Project'
-import { Projects } from '../Projects'
-import { Login } from '../Login'
-import { Header } from '../../components/Header'
-import 'antd/dist/antd.css'
-import './App.css';
-
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-} from "react-router-dom";
+  Redirect,
+} from 'react-router-dom';
+import { Dashboard } from '../Dashboard';
+import { NotFound } from '../NotFound';
+import { NewProject } from '../NewProject';
+import { Project } from '../Project';
+import { Projects } from '../Projects';
+import { Login } from '../Login';
+import { Header } from '../../components/Header';
+import 'antd/dist/antd.css';
+import './App.css';
+import { useGlobal } from '../../state';
 
 function App() {
+  const [global, setGlobal, user] = useGlobal();
   return (
     <>
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/dashboard" component={Dashboard}/>
-        <Route exact path="/login" component={Login}/>
-        <Route exact path="/new" component={NewProject}/>
-        <Route exact path="/projects" component={Projects}/>
-        <Route path="/projects/:id" component={Project}/>
-        <Route component={NotFound}/>
-      </Switch>
-    </Router>
+      <Router>
+        <Header />
+        { user ? <Redirect from="/" to="/dashboard" /> : <Redirect from="/" to="/login" /> }
+        <Switch>
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/new" component={NewProject} />
+          <Route exact path="/projects" component={Projects} />
+          <Route path="/projects/:id" component={Project} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
     </>
-  )
+  );
 }
 
 export default App;
