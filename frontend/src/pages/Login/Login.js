cc3cc4e8cb855c68c9db936e93d65bd12f0560ea
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2020-02-26 14:49:30 -0700

    clean up duplicate token code with api file

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,71 +1,32 @@
-import React, { useState } from 'react';
-import { Redirect } from 'react-router';
-import qs from 'query-string';
-import Axios from 'axios';
-import { Button } from 'antd'; // TODO CONNECT TO EXTERNAL COMPONENTS
-import 'antd/dist/antd.css';
-
+import React from 'react';
+import { Button } from '../../external_components';
+import { useGlobal } from '../../state';
 import './login.css';
 import Logo from '../App/lhl_logo.png';
-import { useGlobal } from '../../state';
-
-const backendEndpoint = 'http://localhost:3022';
-
-const getTokenFromQuery = () => {
-  const queryValues = qs.parse(window.location.search);
-  return queryValues.token;
-};
+import { useLogin } from '../../api';
 
 export const Login = () => {
-  const [loginRedirect, setLoginRedirect] = useState(false);
-  const [loadingSpinner, setLoadingSpinner] = useState(false);
-  const [loadUser, setLoadUser] = useState(false);
-  const [global, setGlobal, user] = useGlobal();
+  const [ login, err, loading, data  ] = useLogin();
+  const [ global, setGlobal ] = useGlobal(); 
 
-  const token = getTokenFromQuery();
-
-  function fetchUser() {
-    setLoadUser(true);
-    Axios.get(`${backendEndpoint}/api/user/profile`, { headers: { Authorization: `Bearer ${token}` } })
-      .then((res) => {
-        setGlobal({ ...global, user: res.data.data });
-        setLoadUser(false);
-      })
-      .catch(() => setLoadUser(false));
-  }
-
-  if (global.token) {
-    return <Redirect to="/projects" />;
-  }
-
-
-  if (token) {
-    if (!user && !loadUser) {
-      fetchUser();
-    }
-    setGlobal({ ...global, token, loginRedirect: true });
-  }
-
-  if (loadingSpinner) {
+  if (loading) {
     return (
-      <div>
+      <div className="loadingSpinner">
         <Button type="primary" loading>
           Loading
         </Button>
       </div>
     );
   }
-
-  if (loginRedirect) {
-    return <Redirect to="/dashboard" />;
-  }
-
   return (
     <main id="login-page">
       <div className="logo">YYC Labs</div>
-      <a href={process.env.REACT_APP_GITHUB_OAUTH_URL}>
+      {/* Temp commented out until oauth is connected properly */}
+      {/* <Button onClick={() => window.location.href=process.env.REACT_APP_GITHUB_OAUTH_URL}> */}
+      <Button onClick={() => setGlobal({ user: 'user1' })}>
         Login
-      </a>
+      </Button>
+      {/* </a> */}
       <p>A GitHub account is required to login to YYC Labs.</p>
       <img alt="logo" src={Logo} height="40" />
     </main>
